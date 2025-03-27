d01bdfbefa580ddc8819a3838f3db99c86abb987
LUIS <luisenriquejuanf@gmail.com>
2025-03-27 11:47:55 +0100

    Refactor BuildCard component: improve code readability and add slot click functionality to remove items

--- src/components/BuildCard.tsx
+++ src/components/BuildCard.tsx
@@ -1,19 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function BuildCard() {
   const [buildItems, setBuildItems] = useState<string[]>(Array(6).fill("")); // Estado para los items de la build
 
-  const handleDrop = (event: React.DragEvent<HTMLDivElement>, index: number) => {
+  const handleDrop = (
+    event: React.DragEvent<HTMLDivElement>,
+    index: number
+  ) => {
     event.preventDefault();
     const itemId = event.dataTransfer.getData("itemId");
     const newBuildItems = [...buildItems];
-    newBuildItems[index] = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${itemId}.png`;
+    newBuildItems[
+      index
+    ] = `https://ddragon.leagueoflegends.com/cdn/15.5.1/img/item/${itemId}.png`;
     setBuildItems(newBuildItems);
   };
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
   };
+
+  const handleSlotClick = (index: number) => {
+    const newBuildItems = [...buildItems];
+    newBuildItems[index] = ""; // Elimina el ítem del slot
+    setBuildItems(newBuildItems);
+  };
+
   return (
     <div className="build-card-container">
       <div className="build-card-header">
@@ -27,22 +39,26 @@ function BuildCard() {
       </div>
       <div className="build-card">
         <div className="build-champion">
-          <img src="https://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/Kaisa.png" alt="Kai'sa"/>
+          <img
+            src="https://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/Kaisa.png"
+            alt="Kai'sa"
+          />
           <h2>Kai'sa</h2>
         </div>
         <div className="build-items-grid">
           {buildItems.map((item, index) => (
-            <div className="build-items-slot"
+            <div
+              className="build-items-slot"
               key={index}
               onDrop={(event) => handleDrop(event, index)}
-              onDragOver={handleDragOver}>
+              onDragOver={handleDragOver}
+              onClick={() => handleSlotClick(index)}
+            >
               {item && <img src={item} alt={`Item ${index + 1}`} />}
             </div>
           ))}
         </div>
-        <div className="build-items-trinket">
-          
-        </div>
+        <div className="build-items-trinket"></div>
       </div>
     </div>
   );
